9999e564488da08f9ee6ba8a7b4d807c757cfd8e
helpful-animation-4719 <um_2003_8@hotmail.com>
2019-10-11 16:59:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -52,11 +52,11 @@ def monopoly_with_fourth_tier
 		# 	 :four_pieces_owned => 200
 		# 	 }
 		:names => {
-			:reading_railroad => {[["mortgage_value"] => "$100"]
-			:pennsylvania_railroad => {["mortgage_value"] => "$200"}
-			:b_and_o_railroad => {["mortgage_value"] => "$400"}
-			:shortline_railroad => {["mortgage_value"] => "$800"}
-		
+			:reading_railroad => {"mortgage_value" => "$100"},
+			:pennsylvania_railroad => {"mortgage_value" => "$200"},
+			:b_and_o_railroad => {"mortgage_value" => "$400"},
+			:shortline_railroad => {"mortgage_value" => "$800"}
+		}
 		 }
 	 }
 	 base_hash# When you start writing the implementation for this method, copy the Hash
